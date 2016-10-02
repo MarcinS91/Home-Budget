6af110f544350ad416775368ab2db205c7903471
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-03 01:30:08 +0200

    commit

--- homeBudget/src/main/java/com/budget/controller/HomeCotroller.java
+++ homeBudget/src/main/java/com/budget/controller/HomeCotroller.java
@@ -1,8 +1,13 @@
 package com.budget.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeCotroller {
 
+	@RequestMapping("/")
+	public String welcome() {
+		return "welcome";
+	}
 }
