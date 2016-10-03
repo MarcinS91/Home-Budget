b3b5ec744c25d0096290a6aaed27fc325615f4c7
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-03 16:14:20 +0200

    commit

--- homeBudget/src/main/java/com/budget/model/Account.java
+++ homeBudget/src/main/java/com/budget/model/Account.java
@@ -11,4 +11,23 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int accountId;
+
+	private double grandTotal;
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public double getGrandTotal() {
+		return grandTotal;
+	}
+
+	public void setGrandTotal(double grandTotal) {
+		this.grandTotal = grandTotal;
+	}
+
 }
