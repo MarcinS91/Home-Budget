e6bd64f3b104647c2f4e0333f045505ecfd3a137
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-03 15:39:36 +0200

    commit

--- homeBudget/src/main/java/com/budget/model/Authorities.java
+++ homeBudget/src/main/java/com/budget/model/Authorities.java
@@ -1,5 +1,35 @@
 package com.budget.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Authorities {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int authoritiesId;
+	private String username;
+	private String authority;
+	
+	public int getAuthoritiesId() {
+		return authoritiesId;
+	}
+	public void setAuthoritiesId(int authoritiesId) {
+		this.authoritiesId = authoritiesId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+	
 }
