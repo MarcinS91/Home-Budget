b4605adaf2fe36bd09249f3455c849cf4e205533
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-03 21:19:23 +0200

    commit

--- homeBudget/src/main/java/com/budget/model/Budget.java
+++ homeBudget/src/main/java/com/budget/model/Budget.java
@@ -1,5 +1,7 @@
 package com.budget.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,4 +13,6 @@ public class Budget {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int budgetId;
+	
+	BigDecimal amount;
 }
