68642323f322122ae492d318d5ec817f5629b8b4
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-05 00:05:56 +0200

    commit

--- homeBudget/src/main/java/com/budget/dao/impl/UsersDaoImpl.java
+++ homeBudget/src/main/java/com/budget/dao/impl/UsersDaoImpl.java
@@ -2,12 +2,14 @@ package com.budget.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.budget.dao.UsersDao;
+import com.budget.model.Authorities;
 import com.budget.model.Users;
 
 @Repository
@@ -19,6 +21,18 @@ public class UsersDaoImpl implements UsersDao {
 
 	public void addUser(Users user) {
 
+		Session session = sessionFactory.getCurrentSession();
+		Users newUser = new Users();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(user.getPassword());
+		newUser.setEnabled(true);
+		newUser.setUsersId(user.getUsersId());
+		
+		Authorities newAuthority = new Authorities();
+		newAuthority.setAuthority("ROLE_USER");
+		session.saveOrUpdate(newUser);
+		session.saveOrUpdate(newAuthority);
+		newAuthority.setUsername(user.getUsername());
 	}
 
 	public void deleteUser(Users user) {
