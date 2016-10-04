6df5d37a9dd8f8afe1243106612f6896c5000867
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-04 23:52:25 +0200

    commit

--- homeBudget/src/main/java/com/budget/dao/impl/UsersDaoImpl.java
+++ homeBudget/src/main/java/com/budget/dao/impl/UsersDaoImpl.java
@@ -1,5 +1,40 @@
 package com.budget.dao.impl;
 
-public class UsersDaoImpl {
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.budget.dao.UsersDao;
+import com.budget.model.Users;
+
+@Repository
+@Transactional
+public class UsersDaoImpl implements UsersDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void addUser(Users user) {
+
+	}
+
+	public void deleteUser(Users user) {
+
+	}
+
+	public Users getUserById(int usersId) {
+
+	}
+
+	public List<Users> getAllUsers() {
+
+	}
+
+	public Users getUserByUsername(String usernmae) {
+
+	}
 
 }
