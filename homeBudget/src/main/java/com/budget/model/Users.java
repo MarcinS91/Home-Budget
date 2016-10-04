6e0ce5084320f9bdc8811dcf0434ef82e8ed41a3
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-05 01:09:11 +0200

    commit

--- homeBudget/src/main/java/com/budget/model/Users.java
+++ homeBudget/src/main/java/com/budget/model/Users.java
@@ -14,31 +14,47 @@ public class Users {
 	private String username;
 	private String password;
 	private Boolean enabled;
-	
+
+	private Account account;
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
 	public int getUsersId() {
 		return usersId;
 	}
+
 	public void setUsersId(int usersId) {
 		this.usersId = usersId;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Boolean getEnabled() {
 		return enabled;
 	}
+
 	public void setEnabled(Boolean enabled) {
 		this.enabled = enabled;
 	}
-	
-	
+
 }
