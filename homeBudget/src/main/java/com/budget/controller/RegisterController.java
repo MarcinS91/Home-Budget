5893b3c959fb22350f3a4862031b309d0b6d22c3
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-05 14:46:27 +0200

    commit

--- homeBudget/src/main/java/com/budget/controller/RegisterController.java
+++ homeBudget/src/main/java/com/budget/controller/RegisterController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.budget.model.Users;
+
 @Controller
 public class RegisterController {
 
 	@RequestMapping("/register")
 	public String registerCustomer(Model model) {
+		Users user = new Users();
+		model.addAttribute("user", user);
+		return "registerUser";
 		
 	
 	}
