44c60f33e640dfc880493e82cc72e3dbb5c5d846
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-05 18:25:11 +0200

    commit

--- homeBudget/src/main/java/com/budget/controller/RegisterController.java
+++ homeBudget/src/main/java/com/budget/controller/RegisterController.java
@@ -1,10 +1,12 @@
 package com.budget.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.budget.model.Users;
+import com.budget.service.UsersService;
 
 @Controller
 public class RegisterController {
@@ -17,4 +19,9 @@ public class RegisterController {
 		
 	
 	}
+	
+	@Autowired
+	private UsersService usersService;
+	
+	
 }
