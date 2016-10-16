23b6a33c85a085822934d725d32a899e8a498e33
MarcinS91 <szewc.marcin91@gmail.com>
2016-10-16 15:10:54 +0200

    commit

--- homeBudget/src/main/java/com/budget/model/Expenditure.java
+++ homeBudget/src/main/java/com/budget/model/Expenditure.java
@@ -16,11 +16,52 @@ public class Expenditure {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int expenditureId;
-	
+
 	@NotEmpty(message = "Nazwa nie może pozostać pusta!")
 	private String expenditureName;
 	private String expenditureData;
 	private String expenditureCategory;
 	@Min(value = 0, message = "Wartość nie może być mniejsza od zera!")
-	BigDecimal expenditureCost;
+	private BigDecimal expenditureCost;
+
+	public int getExpenditureId() {
+		return expenditureId;
+	}
+
+	public void setExpenditureId(int expenditureId) {
+		this.expenditureId = expenditureId;
+	}
+
+	public String getExpenditureName() {
+		return expenditureName;
+	}
+
+	public void setExpenditureName(String expenditureName) {
+		this.expenditureName = expenditureName;
+	}
+
+	public String getExpenditureData() {
+		return expenditureData;
+	}
+
+	public void setExpenditureData(String expenditureData) {
+		this.expenditureData = expenditureData;
+	}
+
+	public String getExpenditureCategory() {
+		return expenditureCategory;
+	}
+
+	public void setExpenditureCategory(String expenditureCategory) {
+		this.expenditureCategory = expenditureCategory;
+	}
+
+	public BigDecimal getExpenditureCost() {
+		return expenditureCost;
+	}
+
+	public void setExpenditureCost(BigDecimal expenditureCost) {
+		this.expenditureCost = expenditureCost;
+	}
+
 }
